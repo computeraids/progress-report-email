95183b324a423a18bf24cbbc46e51b1e0562f9f3
Liam <lcuster@charlotte.edu>
2025-08-28 11:05:39 -0400

    Added comments to main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import funcs
 
 run = True
+
+# this is the most basic thing, but it just runs a interactive terminal thing for the tool. We can interact with any supporting functions from funcs, 
+# but use this file to write software logic.
 while run:
     command = int(input("""What command do you want to use? (Input a number)\n1) Scrape Assignments \t 2) Retrieve API Info
 3)Check Config \t\t 4) Make Emails\n5) Exit\n"""))
